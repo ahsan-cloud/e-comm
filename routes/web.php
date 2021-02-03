b1b9cf9f401174ec5da3713ef97832816057aa7a
ahsan43@gmail.com <relax@gmail.com>
2021-02-03 16:22:41 +0500

    second entry

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
+//use App\Http\Controllers\UserController;
 
+// $resp = HTTP::get("https://jsonplaceholder.typicode.com/posts");
+// //print_r($resp->status());
+// dd($resp->json());
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,7 +18,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::view('/','login');
+ Route::get('/login', function () {
+     return view('login');
+ });
+//Route::view('/','login');
+Route::post('/login','UserController@login');
+Route::get('/','ProductController@index');
